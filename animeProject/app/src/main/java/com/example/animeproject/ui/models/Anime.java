420aa22d38b9e4d7b9e673ddea8c823eade720a3
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-02-11 08:57:02 +0100

    FEAT: create android nav project

---
+++ animeProject/app/src/main/java/com/example/animeproject/ui/models/Anime.java
@@ -0,0 +1,50 @@
+package com.example.animeproject.ui.models;
+
+import java.util.UUID;
+
+public class Anime {
+
+    private UUID id;
+    private String title;
+    private String imageUrl;
+    private String description;
+
+    public Anime(String title, String imageUrl, String description) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.id = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
