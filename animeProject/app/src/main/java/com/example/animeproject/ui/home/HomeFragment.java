59fc7ceb0bffe0299e063b4a1f43dd49afd90cea
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-02-11 19:06:37 +0100

    FEAT: add a button to change view between grid and list

--- animeProject/app/src/main/java/com/example/animeproject/ui/home/HomeFragment.java
+++ animeProject/app/src/main/java/com/example/animeproject/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,6 +20,7 @@ import com.example.animeproject.R;
 import com.example.animeproject.ui.adapters.DataGenerator;
 import com.example.animeproject.ui.adapters.RecyclerViewAdapter;
 import com.example.animeproject.ui.models.Anime;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
@@ -26,6 +29,8 @@ public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
     private RecyclerView recyclerView;
     private RecyclerViewAdapter recyclerViewAdapter;
+    private Button change_button;
+    private RecyclerView.LayoutManager layoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -39,13 +44,32 @@ public class HomeFragment extends Fragment {
             }
         });*/
         recyclerView = root.findViewById(R.id.recyclerview_vertical);
+        change_button = root.findViewById(R.id.change_button);
         initRecyclerView();
+        initListeners();
         return root;
     }
 
     private void initRecyclerView() {
         recyclerViewAdapter = new RecyclerViewAdapter(DataGenerator.generateData(), requireContext());
-        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        layoutManager = new LinearLayoutManager(requireContext());
+        recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    private void initListeners() {
+        change_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (layoutManager instanceof GridLayoutManager) {
+                    layoutManager = new LinearLayoutManager(requireContext());
+                    change_button.setText("Grid View");
+                } else {
+                    layoutManager = new GridLayoutManager(requireContext(), 2);
+                    change_button.setText("List View");
+                }
+                recyclerView.setLayoutManager(layoutManager);
+            }
+        });
     }
 }
