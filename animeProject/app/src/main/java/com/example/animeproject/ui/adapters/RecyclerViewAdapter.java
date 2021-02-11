8173e833671d8a4ca3a3cb2e277ff16fa09db5c0
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-02-11 16:46:54 +0100

    CHORE: show error if image not loaded

--- animeProject/app/src/main/java/com/example/animeproject/ui/adapters/RecyclerViewAdapter.java
+++ animeProject/app/src/main/java/com/example/animeproject/ui/adapters/RecyclerViewAdapter.java
@@ -10,19 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.example.animeproject.R;
 import com.example.animeproject.ui.adapters.viewHolders.AnimeItemViewHolder;
 import com.example.animeproject.ui.models.Anime;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<AnimeItemViewHolder> {
 
-    private ArrayList<Anime> animes = new ArrayList<>();
+    private List<Anime> animes = new ArrayList<>();
     private Context context;
 
 
-    public RecyclerViewAdapter(ArrayList<Anime> animes, Context context) {
+    public RecyclerViewAdapter(List<Anime> animes, Context context) {
         this.animes = animes;
         this.context = context;
     }
@@ -37,7 +39,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<AnimeItemViewHolde
 
     @Override
     public void onBindViewHolder(@NonNull AnimeItemViewHolder holder, int position) {
-        Glide.with(context).asBitmap().load(animes.get(position).getImageUrl()).into(holder.getImage());
+        Glide.with(context).load(animes.get(position).getImageUrl()).error(R.mipmap.ic_launcher)
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(holder.getImage());
         holder.getTitle().setText(animes.get(position).getTitle());
         holder.getDescription().setText(animes.get(position).getDescription());
         holder.getParentLayout().setOnClickListener(new View.OnClickListener() {
