64b08a514e400289359ddbf4511f36fbe0f9924c
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-02-15 00:13:31 +0100

    CHORE: implement the mapper from Anime to AnimeEntity

--- animeProject/app/src/main/java/com/example/animeproject/api/mappers/AnimeToAnimeEntityMapper.java
+++ animeProject/app/src/main/java/com/example/animeproject/api/mappers/AnimeToAnimeEntityMapper.java
@@ -1,15 +1,18 @@
 package com.example.animeproject.api.mappers;
 
+import com.example.animeproject.api.entities.AnimeEntity;
 import com.example.animeproject.ui.models.Anime;
 
 import java.util.List;
 
 import okhttp3.Response;
 
-public class ResponseToAnimeListMapper {
+public class AnimeToAnimeEntityMapper {
 
-    public List<Anime> map(Response response) {
-        System.out.println(">>>>>>> " + response.body().toString());
-        return null;
+    public AnimeEntity map(Anime anime) {
+        AnimeEntity animeEntity = new AnimeEntity();
+        animeEntity.setImageUrl(anime.getImage_url());
+        animeEntity.setTitle(anime.getTitle());
+        return animeEntity;
     }
 }
