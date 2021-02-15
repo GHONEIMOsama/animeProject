fd40797eb91d83e4e6b9eb985abd10ce531876a8
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-02-15 22:55:50 +0100

    CHORE: add annotations

--- animeProject/app/src/main/java/com/example/animeproject/api/entities/AnimeEntity.java
+++ animeProject/app/src/main/java/com/example/animeproject/api/entities/AnimeEntity.java
@@ -1,9 +1,30 @@
 package com.example.animeproject.api.entities;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.UUID;
+
+@Entity
 public class AnimeEntity {
 
+    @PrimaryKey
+    private int uid;
+
+    @ColumnInfo(name = "title")
     private String title;
+
+    @ColumnInfo(name = "image_url")
     private String imageUrl;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
 
     public String getTitle() {
         return title;
