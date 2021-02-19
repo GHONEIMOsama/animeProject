6f8abc85a780f2913874230b913791cb0b25793c
GHONEIM Osama <osama.ghoneim.etu@univ-lille.fr>
2021-02-19 15:46:10 +0100

    FEAT: create logoff fragment

--- animeProject/app/src/main/java/com/example/animeproject/ui/logOff/LogoffFragment.java
+++ animeProject/app/src/main/java/com/example/animeproject/ui/logOff/LogoffFragment.java
@@ -12,12 +12,12 @@ import androidx.fragment.app.Fragment;
 import com.example.animeproject.MainActivity;
 import com.example.animeproject.R;
 
-public class LogOffFragment extends Fragment {
+public class LogoffFragment extends Fragment {
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_logOff, container, false);
+        View root = inflater.inflate(R.layout.fragment_logoff, container, false);
         final Button logOffButton = root.findViewById(R.id.logOff);
         logOffButton.setOnClickListener(new View.OnClickListener() {
             @Override
