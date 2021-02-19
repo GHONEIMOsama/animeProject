72ea570cf62ff093a622066cd636268ad4196b73
GHONEIM Osama <osama.ghoneim.etu@univ-lille.fr>
2021-02-19 15:45:52 +0100

    STYLE: renaming

---
+++ animeProject/app/src/main/java/com/example/animeproject/ui/logOff/LogoffFragment.java
@@ -0,0 +1,30 @@
+package com.example.animeproject.ui.logOff;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.animeproject.MainActivity;
+import com.example.animeproject.R;
+
+public class LogOffFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_logOff, container, false);
+        final Button logOffButton = root.findViewById(R.id.logOff);
+        logOffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+        return root;
+    }
+}
